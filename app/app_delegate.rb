1ac3e2b9345d3bf6a7cff0fb477d27bbb2713df9
Ethan Sherbondy <sherbondye@gmail.com>
2013-10-10 21:46:40 -0400

    working setup.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,7 +1,9 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     alert = UIAlertView.new
-    alert.message = "Hello World!"
+    alert.addButtonWithTitle('Done')
+    alert.cancelButtonIndex = 0
+    alert.message = "Hello there!"
     alert.show
     true
   end
